d1bd5f2c88b628ff8142e43c0934450f4f240e34
DianisMartinez <dianagomez192002@gmail.com>
2024-10-29 00:46:59 +0000

    trabajo

---
+++ src/js/component/Header.jsx
@@ -0,0 +1,26 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item"><a class="nav-link active text-white" aria-current="page" href="#">About</a></li>
+                <li className="nav-item"><a class="nav-link text-white" href="#">Services</a></li>
+                <li className="nav-item"><a class="nav-link text-white" href="#">Portfolio</a></li>
+                <li className="nav-item"><a class="nav-link text-white" href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+  );
+};
+
+export default Header;
---
+++ src/js/component/Main.jsx
@@ -0,0 +1,20 @@
+// src/components/Main.jsx
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="card-fluid m-4 bg-light">
+  <div className="card-body">
+    <h1 className="card-title">A Warm Welcome!</h1>
+    <p className="card-text">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni facere blanditiis asperiores, voluptatum,
+      optio temporibus a ab dicta accusamus, quas animi non. Iste, blanditiis expedita! Animi at vero magnam
+      architecto!
+    </p>
+    <a href="#" className="btn btn-primary">Button</a>
+  </div>
+</div>
+  );
+};
+
+export default Main;
